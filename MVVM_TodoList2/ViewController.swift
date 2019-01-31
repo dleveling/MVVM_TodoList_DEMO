7764f73c16b71c80e614027b84947d67fdcdcc56
cwatpark <chaiwat.sob@spumail.net>
2019-01-31 16:53:22 +0700

    Initial Commit

---
+++ MVVM_TodoList2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM_TodoList2
+//
+//  Created by Ice on 31/1/2562 BE.
+//  Copyright © 2562 Ice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
